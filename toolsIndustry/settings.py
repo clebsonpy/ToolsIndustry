285d47e370156dbaaa2b39be3b4784db84cb7d5e
Clebson Farias <clebson.farias@ctec.ufal.br>
2021-03-16 22:31:00 -0300

    add api docs swagger-ui

--- toolsIndustry/settings.py
+++ toolsIndustry/settings.py
@@ -16,7 +16,7 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
+print(BASE_DIR)
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
@@ -67,7 +67,7 @@ ROOT_URLCONF = 'toolsIndustry.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [str(BASE_DIR / "templates")],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
